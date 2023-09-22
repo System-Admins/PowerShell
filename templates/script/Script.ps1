f4b666fce98ffa11a9aaca9f6667ae973898f3a5
Alex Ø. T. Hansen <94194707+System-Admins-ath@users.noreply.github.com>
2023-09-22 08:44:31 +0200

    Create Script.ps1 (#4)
    Signed-off-by: Alex Ø. T. Hansen <94194707+System-Admins-ath@users.noreply.github.com>

---
+++ templates/script/Script.ps1
@@ -0,0 +1,57 @@
+#Requires -version 5.1;
+
+<#
+.SYNOPSIS
+  .
+
+.DESCRIPTION
+  .
+
+.Parameter MyParameter
+  This parameter is an example.
+
+.Example
+   .\Script.ps1 -Parameter MyParameter;
+
+.NOTES
+  Version:        1.0
+  Author:         Firstname Lastname (initials@systemadmins.com)
+  Creation Date:  DD-MM-YYYY
+  Purpose/Change: Initial script development.
+#>
+
+#region begin boostrap
+############### Bootstrap - Start ###############
+
+[cmdletbinding()]
+
+Param
+(
+)
+
+############### Bootstrap - End ###############
+#endregion
+
+#region begin input
+############### Input - Start ###############
+
+############### Input - End ###############
+#endregion
+
+#region begin functions
+############### Functions - Start ###############
+
+############### Functions - End ###############
+#endregion
+
+#region begin main
+############### Main - Start ###############
+
+############### Main - End ###############
+#endregion
+
+#region begin finalize
+############### Finalize - Start ###############
+
+############### Finalize - End ###############
+#endregion
