7cdfe1c2c092345fadbcff13e4263b743df8a128
Alex Ø. T. Hansen <94194707+System-Admins-ath@users.noreply.github.com>
2025-05-20 12:15:26 +0200

    Update Get-MicrosoftTeamsReport.ps1
    Signed-off-by: Alex Ø. T. Hansen <94194707+System-Admins-ath@users.noreply.github.com>

--- scripts/microsoft/365/teams/get-microsoftteamsreport/Get-MicrosoftTeamsReport.ps1
+++ scripts/microsoft/365/teams/get-microsoftteamsreport/Get-MicrosoftTeamsReport.ps1
@@ -260,6 +260,7 @@ foreach ($msTeam in $msTeams)
         $result.SharePointStorageUsageGB = $spoActivityDetail.'Storage Used (Byte)' / 1GB;
         $result.SharePointPageViewCount = $spoActivityDetail.'Page View Count';
         $result.SharePointTemplate = $spoActivityDetail.'Root Web Template';
+        $result.SharePointLocaleId = $spoSite.LocaleId;
 
         # If last activity date is not empty.
         if (-not [string]::IsNullOrEmpty($spoActivityDetail.'Last Activity Date') )
